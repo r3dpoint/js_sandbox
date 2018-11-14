5b21e8904fce2f9d252120a48cb8fa5eb4d2de7e
redpoint <r3dpoint@users.noreply.github.com>
2018-11-14 08:31:42 -0800

    moved eslint overrides proximal to where they are needed

--- src/rnd_unq_str_gen/index.spec.js
+++ src/rnd_unq_str_gen/index.spec.js
@@ -1,14 +1,14 @@
 import { expect } from 'chai';
 import * as funcs from './index';
 
-describe('generate', function () {
-  it('generates a 6 character alpha numeric string', function() {
+describe('when testing the random unique string module', function () {
+  it('generates a 6 character alpha numeric string', function () {
     let act = funcs.generate();
     expect(act).to.have.a.lengthOf(6);
     expect(act).to.match(/^[a-zA-Z0-9]*$/);
   });
 
-  it.only('generates a unique and random string', function() {
+  it('generates a unique and random string', function () {
     this.timeout(10000);
     let max = 1000000;
     let set = new Set();
@@ -21,19 +21,23 @@ describe('generate', function () {
     expect(set.size).to.equal(max);
   });
 
-  it('generates a (unique and) random string a lot of times', function () {
-    console.log("uniqStrS=" + funcs.generate());
-    let max = 100000000;
-    console.log('max=' + max);
-    let dStart = Date.now();
-    for (let i = 0; i < max; i++) {
-      // console.log(funcs.generate());
-      funcs.generate();
-    }
-    let dEnd = Date.now();
-    console.log('dStart=' + dStart);
-    console.log('dEnd=' + dEnd);
-    console.log('elapsed=' + (dEnd - dStart));
-    console.log("uniqStrE=" + funcs.generate());
+  describe.only('when logging to the console', function () {
+    /* eslint-disable no-console */
+    it('generates a (unique and) random string a lot of times', function () {
+      console.log("uniqStrS=" + funcs.generate());
+      let max = 100000000;
+      console.log('max=' + max);
+      let dStart = Date.now();
+      for (let i = 0; i < max; i++) {
+        // console.log(funcs.generate());
+        funcs.generate();
+      }
+      let dEnd = Date.now();
+      console.log('dStart=' + dStart);
+      console.log('dEnd=' + dEnd);
+      console.log('elapsed=' + (dEnd - dStart));
+      console.log("uniqStrE=" + funcs.generate());
+    });
+    /* eslint-enable no-console */
   });
 });
